9d9767e66412577dd002569463953e533d86e196
advait-soni <advaitsoni2020@gmail.com>
2025-11-29 16:33:26 +0530

    ws connection

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-import express from 'express'
-import 'dotenv/config'
+import "dotenv/config";
 
-const app = express()
+const HOST = "generativelanguage.googleapis.com";
+const PATH =
+  "google.ai.generativelanguage.v1alpha.GenerativeService.BidiGenerateContent";
+const API_KEY = process.env.GEMINI_API_KEY;
 
-const gemini_api_key = process.env.GEMINI_API_KEY
+const URL = `wss://${HOST}/ws/${PATH}?key=${API_KEY}`;
+const MODEL = "gemini-live-2.5-flash-preview";
+const ws = new WebSocket(URL);
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+ws.onopen = () => {
+  console.log("WS connection established");
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-    console.log("api key" , gemini_api_key)
-    
-})
+  const setupMessage = {
+    setup: {
+      model: `models/${MODEL}`,
+      generationConfig: {
+        responseModalities: ["AUDIO"],
+        speechConfig: {
+          voiceConfig: {
+            prebuiltVoiceConfig: {
+              voiceName: "Puck", 
+            },
+          },
+        },
+      },
+    },
+  };
+
+  ws.send(JSON.stringify(setupMessage));
+  console.log("Server is Configured");
+};
+
+ws.onmessage = async (event) => {
+    let msg;
+    try {
+        // msg = JSON.parse(event.data.toString());
+        console.log("event: ", event.data)
+    }
+    catch (e){
+        console.error("Error parsing message not valid json:", e);
+        return;
+    }
+    // if(msg.setupComplete){
+    //     console.log("🤖 SETUP COMPLETE: Model is ready for audio streaming.");
+        
+    //     // 🚨 Next Code Block Goes Here: Start the microphone!
+        
+    //     return;
+    // }
+    // if (msg.serverContent) {
+    //     // console.log("📥 Received conversation content structure...");
+    //     // Audio playback logic will go here
+    // }
+}
+
+ws.onerror = (err) => console.error("WS error:", err);
